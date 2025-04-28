899337891ef690e72b56350aaf7c9fe75ae5e0ef
billClinton048 <clintonkaranja46@gmail.com>
2025-04-29 01:54:17 +0330

    Adde the model2

--- task_manager/app.py
+++ task_manager/app.py
@@ -10,7 +10,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db =SQLAlchemy(app)
 
-class Task(db.Model)
+class Task(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(200), nullable = False)
+	done = db.Column(db.Boolean, default = False)
+
+def to_dict(self):
+	return {
+	'id' : self.id,
+	'title' : self.title,
+	'done' : self.done
+	}
 # Sample data
 tasks =[
 {"id": 1, "title": "Buy Milk", "done": False},
